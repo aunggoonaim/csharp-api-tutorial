22213c3d362c4febe239d931a420bc3e2985c197
Aunggoon Arpasunun <rabbit.dev.group@gmail.com>
2022-12-12 23:06:55 +0700

    initial create

---
+++ Controllers/HelloController.cs
@@ -0,0 +1,43 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace csharp_api_tutorial.Controllers;
+
+[ApiController]
+[Route("Hello")]
+public class HelloController : ControllerBase
+{
+    private static readonly string[] Summaries = new[]
+    {
+        "John", "Arachne", "Chilly", "Boy", "Jo"
+    };
+
+    private readonly ILogger<HelloController> _logger;
+
+    public HelloController(ILogger<HelloController> logger)
+    {
+        _logger = logger;
+    }
+
+    [HttpGet]
+    [Route("")]
+    public IActionResult Get()
+    {
+        return Ok();
+    }
+
+    [HttpGet]
+    [Route("by")]
+    public IActionResult GetByContain([FromQuery] string? search)
+    {
+        var list = new List<int>() { 1, 2, 3, 4, 5 };
+        return Ok(
+            list.Select(index => new WeatherForecast
+            {
+                Fullname = Summaries[index - 1],
+                Sequence = index,
+                IsDeleted = index % 2 == 0
+            })
+            .ToArray()
+        );
+    }
+}
---
+++ WeatherForecast.cs
@@ -0,0 +1,8 @@
+namespace csharp_api_tutorial;
+
+public class WeatherForecast
+{
+    public string Fullname { get; set; } = null!;
+    public int Sequence { get; set; }
+    public bool IsDeleted { get; set; }
+}
